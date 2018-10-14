0206dc1530a0c0f570657751b0c2099b7b7403e6
chedjou03 <chedjou03@yahoo.fr>
2018-10-14 14:24:38 -0400

    update the sql script , add store procedure, with java example to use jdbc to call store procedure with IN, INOUT, OUT parameter

--- sqlScript/table-setup.sql
+++ sqlScript/table-setup.sql
@@ -36,3 +36,50 @@ DROP USER IF EXISTS 'simplice'@'localhost';
 
 CREATE USER 'simplice'@'localhost' IDENTIFIED BY 'test';
 GRANT ALL ON demo.* TO 'simplice'@'localhost';
+
+
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `get_count_for_department`$$
+
+CREATE DEFINER=`student`@`localhost` PROCEDURE `get_count_for_department`(IN the_department VARCHAR(64), OUT the_count INT)
+BEGIN
+	
+	SELECT COUNT(*) INTO the_count FROM employees where department=the_department;
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `get_employees_for_department`$$
+
+CREATE DEFINER=`student`@`localhost` PROCEDURE `get_employees_for_department`(IN the_department VARCHAR(64))
+BEGIN
+
+	SELECT * from employees where department=the_department;
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `greet_the_department`$$
+
+CREATE DEFINER=`student`@`localhost` PROCEDURE `greet_the_department`(INOUT department VARCHAR(64))
+BEGIN
+
+	SET department = concat('Hello to the awesome ', department, ' team!');
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `increase_salaries_for_department`$$
+
+CREATE DEFINER=`student`@`localhost` PROCEDURE `increase_salaries_for_department`(IN the_department VARCHAR(64), IN increase_amount DECIMAL(10,2))
+BEGIN
+
+	UPDATE employees SET salary= salary + increase_amount where department=the_department;
+
+END$$
+DELIMITER ;
+
---
+++ src/CallStoreProcGreetTheDepartment.java
@@ -0,0 +1,55 @@
+import java.sql.*;
+
+
+public class CallStoreProcGreetTheDepartment {
+
+	public static void main(String[] args) throws Exception {
+
+		Connection myConn = null;
+		CallableStatement myStmt = null;
+		
+		String dbUrl = "jdbc:mysql://localhost:3306/demo";
+		String user = "simplice";		
+		String password = "password";
+
+		try {
+			// Get a connection to database
+			myConn = DriverManager.getConnection(dbUrl, user, password);
+
+			String theDepartment = "Engineering";
+			
+			// Prepare the stored procedure call
+			myStmt = myConn
+					.prepareCall("{call greet_the_department(?)}");
+
+			// Set the parameters
+			myStmt.registerOutParameter(1, Types.VARCHAR);
+			myStmt.setString(1, theDepartment);
+
+			// Call stored procedure
+			System.out.println("Calling stored procedure.  greet_the_department('" + theDepartment + "')");
+			myStmt.execute();
+			System.out.println("Finished calling stored procedure");			
+			
+			// Get the value of the INOUT parameter
+			String theResult = myStmt.getString(1);
+			
+			System.out.println("\nThe result = " + theResult);
+
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		} finally {
+			close(myConn, myStmt);
+		}
+	}
+
+	private static void close(Connection myConn, Statement myStmt) throws SQLException {
+		if (myStmt != null) {
+			myStmt.close();
+		}
+
+		if (myConn != null) {
+			myConn.close();
+		}
+	}
+}
